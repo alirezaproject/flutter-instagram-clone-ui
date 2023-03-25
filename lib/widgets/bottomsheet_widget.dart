9af1e7e96dabd2df5e375cbca13c58e9b472e686
alirezaproject <shekoohianproject@gmail.com>
2023-03-25 12:40:28 +0330

    feat : search in bottom sheet

--- lib/widgets/bottomsheet_widget.dart
+++ lib/widgets/bottomsheet_widget.dart
@@ -3,6 +3,7 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:instagram/constants.dart';
 
 class SharedBottomSheet extends StatelessWidget {
   const SharedBottomSheet({super.key, required this.controller});
@@ -10,6 +11,7 @@ class SharedBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var theme = Theme.of(context);
     return ClipRRect(
       borderRadius: BorderRadius.only(
         topLeft: Radius.circular(20),
@@ -21,19 +23,78 @@ class SharedBottomSheet extends StatelessWidget {
           color: Color.fromRGBO(255, 255, 255, 0.1),
           padding: EdgeInsets.symmetric(horizontal: 44),
           height: 300,
-          child: GridView.builder(
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisSpacing: 20,
-              mainAxisSpacing: 20,
-              crossAxisCount: 4,
-            ),
-            itemCount: 30,
+          child: CustomScrollView(
             controller: controller,
-            itemBuilder: (BuildContext context, int index) {
-              return Container(
-                color: Colors.red,
-              );
-            },
+            slivers: [
+              SliverToBoxAdapter(
+                child: Column(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(top: 10, bottom: 22),
+                      height: 5,
+                      width: 67,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(100),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Share',
+                          style: theme.textTheme.headlineSmall,
+                        ),
+                        Image.asset(kImageAsset + 'icon_share_bottomsheet.png'),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 32,
+                    ),
+                    Container(
+                      height: 46,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(13),
+                        color: Color.fromRGBO(255, 255, 255, 0.4),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 8),
+                        child: Row(
+                          children: [
+                            Image.asset(kImageAsset + 'icon_search.png'),
+                            SizedBox(
+                              width: 8,
+                            ),
+                            Expanded(
+                              child: TextField(
+                                decoration: InputDecoration(
+                                    hintText: 'Search User',
+                                    enabledBorder: InputBorder.none,
+                                    focusedBorder: InputBorder.none),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 32,
+                    ),
+                  ],
+                ),
+              ),
+              SliverGrid(
+                delegate: SliverChildBuilderDelegate((context, index) {
+                  return Container(
+                    color: Colors.red,
+                  );
+                }, childCount: 40),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 4,
+                    crossAxisSpacing: 20,
+                    mainAxisSpacing: 20),
+              ),
+            ],
           ),
         ),
       ),
